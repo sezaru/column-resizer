c50a655a1d3af07b858255a25f5c038eb535ab79
Eduardo B. Alexandre <sezdocs@github.com>
2024-10-02 13:03:51 -0300

    Update compilation

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,6 +1,47 @@
-import { defineConfig } from 'tsup';
+import { defineConfig, Options } from 'tsup';
 
-import { iifeConfig, legacyConfig, modernConfig } from '../../scripts/tsup-config';
+const defaultEntry = ['src/index.ts'];
+
+export function modernConfig(opts?: Pick<Options, 'entry'>): Options {
+  return {
+    entry: opts?.entry ?? defaultEntry,
+    format: ['cjs', 'esm'],
+    target: ['chrome91', 'firefox90', 'edge91', 'safari15', 'ios15', 'opera77'],
+    outDir: 'dist/modern',
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    bundle: true,
+    minify: true,
+  };
+}
+
+export function legacyConfig(opts?: Pick<Options, 'entry'>): Options {
+  return {
+    entry: opts?.entry ?? defaultEntry,
+    format: ['cjs', 'esm'],
+    target: ['es6', 'node16'],
+    outDir: 'dist/legacy',
+    dts: true,
+    sourcemap: true,
+    clean: true,
+    bundle: true,
+    minify: true,
+  };
+}
+
+export function iifeConfig(opts: Pick<Options, 'entry'> & { globalName: string }): Options {
+  return {
+    entry: opts.entry ?? defaultEntry,
+    format: 'iife',
+    target: ['chrome91', 'firefox90', 'edge91', 'safari15', 'ios15', 'opera77'],
+    outDir: 'dist',
+    clean: true,
+    bundle: true,
+    minify: true,
+    globalName: opts.globalName,
+  };
+}
 
 export default defineConfig([
   iifeConfig({ globalName: 'ColumnResizerCore' }),
